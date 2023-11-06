a4db4b5b136fa033fee66f50cba00c93b78bc27b
Wojciech Balda <wojtek.balda@gmail.com>
2023-11-06 15:59:48 +0100

    add Dashboard component

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Dashboard = styled.div`
+    max-width: 1280px;
+    width: 100%;
+    height: 100vh;
+    margin: 0 auto;
+    padding: 5%;
+    padding-bottom: 4rem;
+    background-color: #e79494;
+`
+    
+export default Dashboard
